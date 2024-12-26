5be90bbe3ef5e3fc1b3709fd7c7636303bb6ad0d
tasawr01 <tasawarhussain33831@gmail.com>
2024-12-26 22:39:12 +0500

    cors

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors');
+const corsLib = require('cors');  // Renamed import to `corsLib`
 const connectDB = require('../config/db');
 const authRoutes = require('../routes/authRoutes');
 const eventRoutes = require('../routes/eventRoutes');
@@ -8,12 +8,11 @@ const checklistRoutes = require('../routes/checklistRoutes');
 const notificationRoutes = require('../routes/notificationRoutes');
 const userRoutes = require('../routes/userRoutes');
 
-
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({
+app.use(corsLib({  // Use `corsLib` instead of `cors`
   origin: 'https://event-planner-app-frontend-fawn.vercel.app/', // Your Vercel frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
@@ -27,7 +26,6 @@ app.use('/api/events', eventRoutes);
 app.use('/api/checklists', checklistRoutes);
 app.use('/api/notifications', notificationRoutes);
 app.use('/api/users', userRoutes);
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
